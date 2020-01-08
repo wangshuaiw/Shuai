1a79696075c45e2624a500e032255b525a538480
wangshuai <834283444@qq.com>
2020-01-08 18:41:02 +0800

    Add UserRole Controller And UserManage Page

---
+++ Shuai.IdentityServer.V1.0/Controllers/UserRoleController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Shuai.IdentityServer.V1._0.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult UserManage()
+        {
+            return View();
+        }
+    }
+}
